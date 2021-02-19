489b351375eb81507e327de2d7bfdddd29173275
minamilad162 <minamilad939@gmail.com>
2021-02-19 22:35:13 +0200

    last js modification

--- js/user-register-script.js
+++ js/user-register-script.js
@@ -1,4 +1,4 @@
-
+// js + fetch
 function myFunction() {
     var x = document.getElementById("pass");
     var z = document.getElementById("re-pass");
@@ -13,21 +13,16 @@ function myFunction() {
         x.type = "password";
     }
 }
-function myFunction2() {
+// function myFunction2() {
 
-}
+// }
 
 async function verifyRegister() {
     var userData = {};
 
-    userData.username = document.getElementById("First-name").value;
-    userData.lname = document.getElementById("last-name").value;
+    userData.username = document.getElementById("username").value;
     userData.password = document.getElementById("pass").value;
-    userData.repassword = document.getElementById("re-pass").value;
-    userData.birthdate = document.getElementById("Birth Date").value;
-    userData.phone = document.getElementById("Phone number").value;
-    userData.Gender = document.getElementById("Gender").value;
-
+    
     const url = 'https://whispering-journey-12121.herokuapp.com/http://anyservice.imassoft.com/9/register';
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
@@ -40,21 +35,50 @@ async function verifyRegister() {
         body: raw,
         redirect: 'follow'
     };
-    let tokenarray = []
-
 
     let httpResponse = await fetch(url, requestOptions)
     responseJsonObj = await httpResponse.json()
     console.log(responseJsonObj)
-    let token =  await responseJsonObj.token
+    let token = await responseJsonObj.token
 
-
-    // verifyRegister();
-    let localStorageUrlArray = window.localStorage.setItem("Token",token)
-
-    let localStoragetoken = window.localStorage.getItem("Token")
-    console.log(localStoragetoken)
-    // document.getElementById("video-container-1").setAttribute("src", localStorageUrlArray[0])
-    // document.getElementById("video-container-2").setAttribute("src", localStorageUrlArray[1])
-
+    // local storage;
+    window.localStorage.setItem("Token", token)
+    console.log(responseJsonObj.id)
+   //login request to get id
+    id = login()
+    addDatatoDB(id) 
 }
+
+async function addDatatoDB(id) {
+    var useradd = {};
+    useradd.username = document.getElementById("username").value;
+    useradd.fname = document.getElementById("First-name").value;
+    useradd.lname = document.getElementById("last-name").value;
+    useradd.password = document.getElementById("pass").value;
+    useradd.repassword = document.getElementById("re-pass").value;
+    useradd.birthdate = document.getElementById("Birth Date").value;
+    useradd.phone = document.getElementById("Phone number").value;
+    useradd.Gender = document.getElementById("Gender").value;
+    useradd.regId = id;
+
+    const url = 'https://whispering-journey-12121.herokuapp.com/http://anyservice.imassoft.com/9/users';
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify(useradd);
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    let httpResponse = await fetch(url, requestOptions)
+    responseJsonObj = await httpResponse.json()
+    console.log(responseJsonObj)
+    let localStoragetoken = window.localStorage.getItem("Token")
+}
+   function login(){
+
+   }
