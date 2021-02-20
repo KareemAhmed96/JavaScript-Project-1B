34a56c1b910461423c46038c14d4799132a0c2f2
Eng-HazemMansour <h.m.mansour97@gmail.com>
2021-02-21 01:25:17 +0200

    code edit

--- js/edit-account-script.js
+++ js/edit-account-script.js
@@ -129,35 +129,6 @@ function validate() {
     return true
 }
 
-    /*let userName = document.getElementById("username").value
-        let userPassword = document.getElementById("password").value
-        /// Formatting the body 
-        var raw =`{"username":"${userName}","password": "${userPassword}" }`
-        // Formatting the Request Options
-        var requestOptions = {
-            method: 'POST',
-            headers: myHeaders,
-            body: raw,
-            redirect: 'follow'
-        };
-      // Getting API Response 
-      let response = await fetch("https://desolate-ocean-66919.herokuapp.com/http://anyservice.imassoft.com/5/login", requestOptions);
-      // Cast Response into JSON Object 
-      let responseJsonObj= await response.json()
-      // Printing Response 
-      console.log(responseJsonObj)
-      // Check the Response 
-      if (responseJsonObj.token){
-        // Successful Login >> Store Token In Locale Storage
-        console.log(responseJsonObj.token)
-        window.localStorage.setItem("Token",responseJsonObj.token)
-      }
-     
-    
-    if(responseJsonObj.token){
-        alert("Data changed successfully");
-    }
-    */
 
   
     
