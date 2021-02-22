e312bf26451f835926bb8fc638c5ee0afd986e2e
kareemzak <kareemahmed96@outlook.com>
2021-02-22 21:15:43 +0200

    added styles for display-video page

--- js/display-video-script.js
+++ js/display-video-script.js
@@ -1,69 +1,195 @@
-var vid, playbtn, seekslider, curtimetext, durtimetext, mutebtn, volumeslider;
-
-function intializePlayer() {
-  // Set object references
-  vid = document.getElementById("my_video");
-  playbtn = document.getElementById("playpausebtn");
-  seekslider = document.getElementById("seekslider");
-  curtimetext = document.getElementById("curtimetext");
-  durtimetext = document.getElementById("durtimetext");
-  mutebtn = document.getElementById("mutebtn");
-  volumeslider = document.getElementById("volumeslider");
-
-  // Add event listeners
-  playbtn.addEventListener("click", playPause, false);
-  seekslider.addEventListener("change", vidSeek, false);
-  vid.addEventListener("timeupdate", seektimeupdate, false);
-  mutebtn.addEventListener("click", vidmute, false);
-  volumeslider.addEventListener("change", setvolume, false);
-}
-window.onload = intializePlayer;
-
-function playPause() {
-  if (vid.paused) {
-    vid.play();
-    playbtn.innerHTML = "Pause";
-  } else {
-    vid.pause();
-    playbtn.innerHTML = "Play";
-  }
-}
-
-function vidSeek() {
-  var seekto = vid.duration * (seekslider.value / 100);
-  vid.currentTime = seekto;
-}
-
-function seektimeupdate() {
-  var nt = vid.currentTime * (100 / vid.duration);
-  seekslider.value = nt;
-  var curmins = Math.floor(vid.currentTime / 60);
-  var cursecs = Math.floor(vid.currentTime - curmins * 60);
-  var durmins = Math.floor(vid.duration / 60);
-  var dursecs = Math.floor(vid.duration - durmins * 60);
-  if (cursecs < 10) { cursecs = "0" + cursecs; }
-  if (dursecs < 10) { dursecs = "0" + dursecs; }
-  if (curmins < 10) { curmins = "0" + curmins; }
-  if (durmins < 10) { durmins = "0" + durmins; }
-  curtimetext.innerHTML = curmins + ":" + cursecs;
-  durtimetext.innerHTML = durmins + ":" + dursecs;
-}
-
-function vidmute() {
-  if (vid.muted) {
-    vid.muted = false;
-    mutebtn.innerHTML = "Mute";
-  } else {
-    vid.muted = true;
-    mutebtn.innerHTML = "Unmute";
-  }
-}
-
-function setvolume() {
-  vid.volume = volumeslider.value / 100;
-}
-
 let token = window.localStorage.getItem("token")
+
+async function logout(){
+  var myHeaders=new  Headers()
+  myHeaders.append("Content-Type","application/json");
+  let token=localStorage.getItem("token")
+  myHeaders.append("token", token)
+
+  var requestOptions={
+      method:'GET',
+      headers:myHeaders,
+      redirect:'follow'
+  }
+  response=await  fetch("https://desolate-ocean-66919.herokuapp.com/http://anyservice.imassoft.com/3/logout",requestOptions)
+  responseObj = await response.json()
+  console.log(responseObj)
+  //Remove token
+  window.localStorage.removeItem("token")
+  //Change status to anyone
+  window.localStorage.setItem("user-status", "anyone")
+  window.location.replace("home.html");
+}
+
+document.addEventListener("keyup",function(e){
+  if(e.key==" "){
+      toggle();
+  }
+  else if(e.key=="ArrowRight"){
+      myVideo.currentTime+=2;
+  }
+  else if(e.key=="ArrowLeft"){
+      myVideo.currentTime-=2;
+  }
+});
+
+let videoId=1;
+
+let myVideo = document.getElementById("myvideo");
+let progressValue = document.getElementById("progressValue");
+let progressBackground = document.getElementById("progressBackground");
+let overlay = document.getElementById("overlay");
+let videoSpeed = document.getElementById("videoSpeed");
+let subtitleDiv = document.getElementById("subtitle");
+let volumeSlider = document.getElementById("myRange");
+
+volumeSlider.addEventListener("mousemove", setVolume);
+
+progressBackground.addEventListener("click",function(e){
+  let maxWidth = progressBackground.clientWidth;
+  let barValue = e.offsetX;
+  let barValuePercent = barValue/maxWidth;
+  let currentTime = myVideo.duration * barValuePercent;
+  myVideo.currentTime = currentTime;
+})
+
+function getProgressBarWidth(){
+  return new Promise(function(resolve,reject){
+      
+          let t = setInterval(function(){
+              if(progressBackground.clientWidth>0){
+                  clearInterval(t);
+                  resolve(progressBackground.clientWidth);
+              }
+          },10);
+  });
+}
+
+myVideo.addEventListener("loadedmetadata",async function(e){
+  console.log(myVideo.duration);
+
+  if(localStorage["video"+videoId]){
+      myVideo.currentTime = Number(localStorage["video"+videoId]);
+
+      let maxWidth = await getProgressBarWidth();
+  
+      console.log(maxWidth,(myVideo.currentTime/myVideo.duration) *maxWidth)
+      progressValue.style.width = (myVideo.currentTime/myVideo.duration) *maxWidth ;
+  
+  }
+  //myProgressBar.max = myVideo.duration;
+})
+
+let subtitlesArray = [{
+  fromTime:1,
+  toTime:2,
+},{
+  fromTime:2,
+  toTime:5,
+},{
+  fromTime:5,
+  toTime:9,
+},{
+  fromTime:9,
+  toTime:12,
+}]
+
+myVideo.addEventListener("timeupdate",function(e){
+  localStorage["video"+videoId] = myVideo.currentTime;
+  
+  let subtitles = subtitlesArray.filter((item)=>
+      myVideo.currentTime>=item.fromTime &&
+      myVideo.currentTime<=item.toTime
+  );
+  if(subtitles.length>0){
+      let subtitle = subtitles[0];
+      //subtitleDiv.innerHTML = subtitle.text;
+  }
+  let maxWidth = progressBackground.clientWidth;
+  progressValue.style.width = (myVideo.currentTime/myVideo.duration) *maxWidth ;
+})
+
+function toggle(){
+  if(myVideo.paused){
+      play();
+  }
+  else
+  {   
+      pause();
+      //Show in overlay
+      showPlayOverlayBtn()
+  }
+}
+
+function play(){
+  myVideo.play();
+  overlay.style.display="none";
+}
+
+function stop(){
+  pause();
+  myVideo.currentTime = "0";
+}
+
+function pause(){
+  myVideo.pause();
+  overlay.style.display="block";
+  //Show in overlay
+  showPlayOverlayBtn()
+}
+
+function moveForward(){
+  myVideo.currentTime += 20;
+}
+
+function videoSpeedChanged(){
+  myVideo.playbackRate = Number(videoSpeed.value);
+}
+
+function fullScreen()
+{
+  myVideo.webkitEnterFullScreen();
+}
+
+function showPlayOverlayBtn() {
+  document.getElementById("pause-play").setAttribute("src", "./imgs/play-button-resized.png");
+}
+
+function showPauseOverlayBtn() {
+  document.getElementById("pause-play").setAttribute("src", "./imgs/pause-button-resized.png");
+}
+
+function showControls(){
+  controls.style.display="block";
+  if(myVideo.paused) {
+      //Show in overlay
+      showPlayOverlayBtn()
+  }
+  else {
+      overlay.style.display="block";
+      //Show in overlay
+      showPauseOverlayBtn()
+  }                
+}
+
+function hideControls(){
+  //controls.style.display="none";
+  if(myVideo.paused) {
+      //Show in overlay
+      showPauseOverlayBtn()
+  }
+  else {
+      overlay.style.display="none";
+      //Show in overlay
+      showPlayOverlayBtn()
+  } 
+}
+
+function setVolume(){
+myVideo.volume = volumeSlider.value / 100;
+}
+
+console.log({myVideo});
 
 async function getVideo() {
   var myHeaders = new Headers();
@@ -81,7 +207,7 @@ async function getVideo() {
 
   responseJsonObj = await httpResponse.json();
   //window.localStorage.setItem("video-url", responseJsonObj.data.url)
-  document.getElementById("my_video").setAttribute("src", responseJsonObj.data.url)
+  document.getElementById("myvideo").setAttribute("src", responseJsonObj.data.url)
 }
 
 //let localStorageUrl = window.localStorage.getItem("video-url")
