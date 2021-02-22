96318d63d849941bf6326a9f51c75471e100d24b
kareemzak <kareemahmed96@outlook.com>
2021-02-22 19:56:48 +0200

    changed video-dashboard styles

--- js/videos-dashboard-script.js
+++ js/videos-dashboard-script.js
@@ -1,68 +1,3 @@
-var vid, playbtn, seekslider, curtimetext, durtimetext, mutebtn, volumeslider;
-
-function intializePlayer() {
-  // Set object references
-  vid = document.getElementById("my_video");
-  playbtn = document.getElementById("playpausebtn");
-  seekslider = document.getElementById("seekslider");
-  curtimetext = document.getElementById("curtimetext");
-  durtimetext = document.getElementById("durtimetext");
-  mutebtn = document.getElementById("mutebtn");
-  volumeslider = document.getElementById("volumeslider");
-  // Add event listeners
-  playbtn.addEventListener("click", playPause, false);
-  seekslider.addEventListener("change", vidSeek, false);
-  vid.addEventListener("timeupdate", seektimeupdate, false);
-  mutebtn.addEventListener("click", vidmute, false);
-  volumeslider.addEventListener("change", setvolume, false);
-}
-
-window.onload = intializePlayer;
-
-function playPause() {
-  if (vid.paused) {
-    vid.play();
-    playbtn.innerHTML = "Pause";
-  } else {
-    vid.pause();
-    playbtn.innerHTML = "Play";
-  }
-}
-
-function vidSeek() {
-  var seekto = vid.duration * (seekslider.value / 100);
-  vid.currentTime = seekto;
-}
-
-function seektimeupdate() {
-  var nt = vid.currentTime * (100 / vid.duration);
-  seekslider.value = nt;
-  var curmins = Math.floor(vid.currentTime / 60);
-  var cursecs = Math.floor(vid.currentTime - curmins * 60);
-  var durmins = Math.floor(vid.duration / 60);
-  var dursecs = Math.floor(vid.duration - durmins * 60);
-  if (cursecs < 10) { cursecs = "0" + cursecs; }
-  if (dursecs < 10) { dursecs = "0" + dursecs; }
-  if (curmins < 10) { curmins = "0" + curmins; }
-  if (durmins < 10) { durmins = "0" + durmins; }
-  curtimetext.innerHTML = curmins + ":" + cursecs;
-  durtimetext.innerHTML = durmins + ":" + dursecs;
-}
-
-function vidmute() {
-  if (vid.muted) {
-    vid.muted = false;
-    mutebtn.innerHTML = "Mute";
-  } else {
-    vid.muted = true;
-    mutebtn.innerHTML = "Unmute";
-  }
-}
-
-function setvolume() {
-  vid.volume = volumeslider.value / 100;
-}
-
 async function fetchAllVideos() {
 
   let token = window.localStorage.getItem("token")
@@ -92,14 +27,7 @@ function renderVideo(id, src, title) {
 
   let card = `<div class="card" onclick="reply_click()">
                       <div class="row _thumbnail text-center"><a href="display-video.html"><img id="${id}" src="${src}"></a></div>
-                      <div class="row text-center"><h3>${title}</h3></div>
-                      <div class="row _text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In semper sapien non
-                          neque finibus, sit amet cursus est faucibus.</div>
-                      <div class="btn-group thumb-btn" role="group" aria-label="...">
-                          <button type="button" class="btn btn-default">View</button>
-                          <button type="button" class="btn btn-default">Star</button>
-                      </div>
-                      <span class="minutes">90 mins</span>
+                      <div class="row text-center"><h3>${title.toUpperCase()}</h3></div>
               </div>`
   
   // Generate event listener for each card -> get IDs
@@ -139,16 +67,6 @@ async function getMovies() {
 //getMovies();
 fetchAllVideos()
 
-let localStorageUrlArray = window.localStorage.getItem("moives-url-array")
-
-localStorageUrlArray = localStorageUrlArray.split(",")
-console.log(localStorageUrlArray[0])
-console.log(localStorageUrlArray[1])
-console.log(localStorageUrlArray[2])
-
-document.getElementById("movie_1").setAttribute("src", localStorageUrlArray[0])
-document.getElementById("movie_2").setAttribute("src", localStorageUrlArray[1])
-document.getElementById("movie_3").setAttribute("src", localStorageUrlArray[2])
 
 
 
